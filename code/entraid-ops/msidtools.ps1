c3377e734871dcd8d82f24ae7ef8550cd415b767
Anders Kristiansen <akristiansen@gmail.com>
2024-10-17 09:43:32 +0200

    install tools

---
+++ code/entraid-ops/msidtools.ps1
@@ -0,0 +1,6 @@
+#install https://www.powershellgallery.com/packages/MSIdentityTools/2.0.70
+
+Install-Module -Name MSIdentityTools -Force -AllowClobber
+
+#load module
+Import-Module MSIdentityTools
